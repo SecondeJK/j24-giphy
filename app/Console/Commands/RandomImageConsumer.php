5dd3ade357865a36635316ae95bd00145cd3fe3a
Jim Seconde <jim.seconde@googlemail.com>
2020-02-20 08:10:58 +0000

    definately failing jobs, but not enough time to work out Bus fake

--- app/Console/Commands/RandomImageConsumer.php
+++ app/Console/Commands/RandomImageConsumer.php
@@ -2,24 +2,23 @@
 
 namespace App\Console\Commands;
 
-use App\Services\GiphyApi;
 use Illuminate\Console\Command;
 
-class TestApi extends Command
+class RandomImageConsumer extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'test:api';
+    protected $signature = 'index:media';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'interactive testing for api, to be depricated after branch is merged';
+    protected $description = 'Attempts to index 500 media objects';
 
     /**
      * Create a new command instance.
@@ -38,7 +37,6 @@ class TestApi extends Command
      */
     public function handle()
     {
-        $api = new GiphyApi();
-        dump($api->getMostRecentGifs(2));
+        //
     }
 }
---
+++ database/migrations/2020_02_20_073618_add_media_entity_migration.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddMediaEntityMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->string('type');
+            $table->string('slug');
+            $table->string('url');
+            $table->string('bitly_url');
+            $table->string('embed_url');
+            $table->string('username');
+            $table->string('source');
+            $table->string('content_url')->nullable();
+            $table->string('source_post_url');
+            $table->dateTime('update_datetime');
+            $table->dateTime('create_datetime');
+            $table->dateTime('import_datetime');
+            $table->dateTime('trending_datetime');
+            // not going to bother with adding a foreighn key index constraint but would do another migration to do one
+            $table->integer('image_id');
+            $table->string('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('media');
+    }
+}
