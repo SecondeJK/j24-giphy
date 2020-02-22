8ad3f40fc7264aecc9baa5b655aebca84182765b
Jim Seconde <jim.seconde@googlemail.com>
2020-02-22 15:09:32 +0000

    complete vue scaffold, import bootstrap, created search component

---
+++ resources/js/components/Search.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        </br>
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-10 col-lg-8">
+                <div class="card card-sm">
+                    <div class="card-body row no-gutters align-items-center">
+                        <div class="col-auto">
+                            <i class="fas fa-search h4 text-body"></i>
+                        </div>
+                        <div class="col">
+                            <input class="form-control form-control-lg form-control-borderless" type="search" v-model=searchTerm placeholder="Search for Gifs">
+                        </div>
+                        <div class="col-auto">
+                            <button class="btn btn-lg btn-success" @click="doSearch">Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                searchTerm: ""
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        },
+        methods: {
+            doSearch() {
+                console.log(this.searchTerm);
+            }
+        }
+    }
+</script>
